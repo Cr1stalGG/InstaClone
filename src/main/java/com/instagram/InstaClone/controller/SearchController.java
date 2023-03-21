eab7166af8d71a29893e34e22a36c2b60bd1ef6f
Alexander_Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-03-22 00:53:14 +0300

    Refactoring Refactor search system

--- src/main/java/com/instagram/InstaClone/controller/SearchController.java
+++ src/main/java/com/instagram/InstaClone/controller/SearchController.java
@@ -3,10 +3,7 @@ package com.instagram.InstaClone.controller;
 import com.instagram.InstaClone.dto.UserMainDataDTO;
 import com.instagram.InstaClone.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +13,7 @@ import java.util.List;
 public class SearchController {
     private final UserService userService;
 
-    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    @GetMapping("/{username}")
     public List<UserMainDataDTO> findAllByUsername(@PathVariable String username){
         return userService.findAllByUsername(username);
     }
